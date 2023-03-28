dc86df5643fba41bb63f1574b690f3d23a27ad06
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-03-28 20:28:35 +0700

    feat: add wrapper class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="font-bold text-2xl text-stone-800">Hello ReadmeBox</h1>
+      <main className="wrapper">
+        <div className="min-h-[90vh] py-4">
+          <h1 className="font-bold text-[28px] text-white">ReadmeBox</h1>
+        </div>
       </main>
     </>
   );
