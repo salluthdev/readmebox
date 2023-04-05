e09c7d726c657b3c80dd8a53affd7d7998463aa3
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-04-05 13:34:11 +0700

    style(result): styling result text

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -16,24 +16,33 @@ const char: charType = {
 
 export default function Result({ text }: ResultProps) {
   const charToRender = Array.from(text).map((c, index) => (
-    <React.Fragment key={index}>{char[c]}</React.Fragment>
+    <React.Fragment key={index}>
+      {char[c]}
+      {c === " " && <span>&nbsp;</span>}
+    </React.Fragment>
   ));
 
   return (
-    <svg
-      fill="none"
-      viewBox="0 0 240 240"
-      width="240"
-      height="240"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <foreignObject
-        width="100%"
-        height="100%"
-        xmlns="http://www.w3.org/1999/xhtml"
+    <div className="flex justify-end">
+      <svg
+        fill="none"
+        viewBox="0 0 240 120"
+        width="240"
+        height="120"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <div className="font-bold text-lg text-white">{charToRender}</div>
-      </foreignObject>
-    </svg>
+        <foreignObject
+          width="100%"
+          height="100%"
+          xmlns="http://www.w3.org/1999/xhtml"
+        >
+          <div className="h-full flex flex-wrap justify-center items-center">
+            <div className="font-bold text-lg text-center break-all text-white">
+              {charToRender}
+            </div>
+          </div>
+        </foreignObject>
+      </svg>
+    </div>
   );
 }
