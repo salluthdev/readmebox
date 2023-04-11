f968e010e0ade69f9386b3f9333866f032bd97fa
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-04-11 12:01:22 +0700

    refactor(search-input): mapping input by inputList

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -11,22 +11,33 @@ export default function SearchInput({
   textTwo,
   setTextTwo,
 }: SearchInputProps) {
+  const inputList = [
+    {
+      placeholder: "FIRST TEXT",
+      state: textOne,
+      setState: setTextOne,
+    },
+    {
+      placeholder: "SECOND TEXT",
+      state: textTwo,
+      setState: setTextTwo,
+    },
+  ];
+
   return (
     <div className="flex items-center gap-2">
-      <input
-        type="text"
-        placeholder="FIRST TEXT"
-        value={textOne}
-        onChange={(e) => setTextOne(e.target.value.toUpperCase())}
-        className="w-full h-10 font-bold rounded-sm outline-none py-1 px-2 mt-1"
-      />
-      <input
-        type="text"
-        placeholder="SECOND TEXT"
-        value={textTwo}
-        onChange={(e) => setTextTwo(e.target.value.toUpperCase())}
-        className="w-full h-10 font-bold rounded-sm outline-none py-1 px-2 mt-1"
-      />
+      {inputList.map((input) => {
+        return (
+          <input
+            key={input.placeholder}
+            type="text"
+            placeholder={input.placeholder}
+            value={input.state}
+            onChange={(e) => input.setState(e.target.value.toUpperCase())}
+            className="w-full h-10 font-bold rounded-sm outline-none py-1 px-2 mt-1"
+          />
+        );
+      })}
     </div>
   );
 }
