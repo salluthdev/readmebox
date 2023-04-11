019f617a7d00720bf3619230f47d268b2965e930
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-04-11 14:01:12 +0700

    refactor(char): simplify css style

--- src/components/char/s.tsx
+++ src/components/char/s.tsx
@@ -1,93 +1,69 @@
-import { CSSProperties } from "react";
-
-type FlexDirection = "row" | "row-reverse" | "column" | "column-reverse";
-
 export default function S() {
-  const charContainer: CSSProperties & { flexDirection: FlexDirection } = {
-    display: "flex",
-    flexDirection: "column",
-    gap: "3px",
-  };
-  const line = {
-    display: "flex",
-    gap: "3px",
-  };
-  const box = {
-    width: "11px",
-    height: "11px",
-    background: "#3aad52",
-    borderRadius: "2px",
-  };
-  const emptyBox = {
-    width: "11px",
-    height: "11px",
-  };
-
   return (
-    <div style={charContainer}>
-      <div style={line}>
-        <span style={emptyBox} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={emptyBox} />
+    <div className="char-container">
+      <div className="line">
+        <span className="box" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box" />
       </div>
-      <div style={line}>
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={emptyBox} />
+      <div className="line">
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box" />
       </div>
-      <div style={line}>
-        <span style={box} />
-        <span style={box} />
-        <span style={emptyBox} />
-        <span style={emptyBox} />
-        <span style={emptyBox} />
-        <span style={emptyBox} />
+      <div className="line">
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box" />
+        <span className="box" />
+        <span className="box" />
+        <span className="box" />
       </div>
-      <div style={line}>
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={emptyBox} />
-        <span style={emptyBox} />
+      <div className="line">
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box" />
+        <span className="box" />
       </div>
-      <div style={line}>
-        <span style={emptyBox} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={emptyBox} />
+      <div className="line">
+        <span className="box" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box" />
       </div>
-      <div style={line}>
-        <span style={emptyBox} />
-        <span style={emptyBox} />
-        <span style={emptyBox} />
-        <span style={box} />
-        <span style={box} />
-        <span style={emptyBox} />
+      <div className="line">
+        <span className="box" />
+        <span className="box" />
+        <span className="box" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box" />
       </div>
-      <div style={line}>
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={emptyBox} />
+      <div className="line">
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box" />
       </div>
-      <div style={line}>
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={box} />
-        <span style={emptyBox} />
-        <span style={emptyBox} />
+      <div className="line">
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box active" />
+        <span className="box" />
+        <span className="box" />
       </div>
     </div>
   );
