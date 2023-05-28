abdaa5f4c82b6eff1ad936cac5c1ee259ea3d00c
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-05-28 18:11:03 +0700

    feat(char/m): fix ui

--- src/components/pages/home/char/m.tsx
+++ src/components/pages/home/char/m.tsx
@@ -34,9 +34,9 @@ export default function M() {
       <div className="line">
         <span className="box active" />
         <span className="box active" />
-        <span className="box" />
         <span className="box active" />
-        <span className="box" />
+        <span className="box active" />
+        <span className="box active" />
         <span className="box active" />
         <span className="box active" />
         <span className="box" />
@@ -45,7 +45,7 @@ export default function M() {
         <span className="box active" />
         <span className="box active" />
         <span className="box" />
-        <span className="box" />
+        <span className="box active" />
         <span className="box" />
         <span className="box active" />
         <span className="box active" />
