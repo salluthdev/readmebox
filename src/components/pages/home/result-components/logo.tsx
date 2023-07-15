3af54c9f75114e625071cc981b4243129b3481bf
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-07-15 13:34:06 +0700

    feat: add decoration logo

--- src/components/pages/home/result-components/logo.tsx
+++ src/components/pages/home/result-components/logo.tsx
@@ -1,43 +1,342 @@
 export default function Logo() {
   return (
-    <svg
-      width="109"
-      height="53"
-      viewBox="0 0 109 53"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect x="14" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="28" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="42" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="56" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="70" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="84" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect y="14" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="14" y="14" width="11" height="11" rx="2" fill="#3AAD52" />
-      <path
-        d="M28 16C28 14.8954 28.8954 14 30 14H37C38.1046 14 39 14.8954 39 16V23C39 24.1046 38.1046 25 37 25H30C28.8954 25 28 24.1046 28 23V16Z"
-        fill="#1B4332"
-      />
-      <rect x="42" y="14" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="56" y="14" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="70" y="14" width="11" height="11" rx="2" fill="#1B4332" />
-      <rect x="84" y="14" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="98" y="14" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect y="28" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="14" y="28" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="28" y="28" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="42" y="28" width="11" height="11" rx="2" fill="#1B4332" />
-      <rect x="56" y="28" width="11" height="11" rx="2" fill="#1B4332" />
-      <rect x="70" y="28" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="84" y="28" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="98" y="28" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="14" y="42" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="28" y="42" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="42" y="42" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="56" y="42" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="70" y="42" width="11" height="11" rx="2" fill="#3AAD52" />
-      <rect x="84" y="42" width="11" height="11" rx="2" fill="#3AAD52" />
-    </svg>
+    <>
+      <style>
+        {`
+        .logo-clip-container {
+          width: 100%;
+          height: 100%;
+					position: relative;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+				}
+        .logo-clip-container .logo {
+          width: 109px;
+          height: 53px;
+          display: flex;
+          flex-direction: column;
+          gap: 3px;
+        }
+        .logo-clip-container .line {
+          display: flex;
+          gap: 3px;
+        }
+        .logo-clip-container .line .box {
+          width: 11px;
+          height: 11px;
+          background: #161b22;
+          border-radius: 2px;
+        }
+        .logo-clip-container .line .box.active {
+          background: #3aad52;
+        }
+        .logo-clip-container .line .box.active-1 {
+          background: #1b4332;
+        }
+        .logo-clip-container .line .box.active-2 {
+          background: #00603c;
+        }
+        .logo-clip-container .decoration {
+          width: 39px;
+          height: 39px;
+          position: absolute;
+          display: flex;
+          flex-direction: column;
+          gap: 3px;
+        }
+        .logo-clip-container .decoration.decoration-1 {
+          top: 28px;
+          left: 70px;
+        }
+        .logo-clip-container .decoration.decoration-2 {
+          bottom: 28px;
+          left: 182px;
+        }
+        .logo-clip-container .decoration.decoration-3 {
+          top: 28px;
+          right: 252px;
+        }
+        .logo-clip-container .decoration.decoration-4 {
+          bottom: 28px;
+          right: 154px;
+        }
+        .logo-clip-container .decoration.decoration-5 {
+          top: 28px;
+          right: 42px;
+        }
+        .logo-clip-container .decoration.decoration-1 .pattern-1 {
+          animation: anim-pattern-1 0.6s infinite;
+        }
+        .logo-clip-container .decoration.decoration-2 .pattern-1 {
+          animation: anim-pattern-1 0.6s 0.4s infinite;
+        }
+        .logo-clip-container .decoration.decoration-3 .pattern-1 {
+          animation: anim-pattern-1 0.6s 0.2s infinite;
+        }
+        .logo-clip-container .decoration.decoration-4 .pattern-1 {
+          animation: anim-pattern-1 0.6s infinite;
+        }
+        .logo-clip-container .decoration.decoration-5 .pattern-1 {
+          animation: anim-pattern-1 0.6s 0.4s infinite;
+        }
+        @keyframes anim-pattern-1 {
+          0%, 50% {
+            opacity: 1;
+          }
+          51%, 100% {
+            opacity: 0;
+          }
+        }
+        .logo-clip-container .decoration.decoration-1 .pattern-2 {
+          animation: anim-pattern-2 0.6s infinite;
+        }
+        .logo-clip-container .decoration.decoration-2 .pattern-2 {
+          animation: anim-pattern-2 0.6s 0.4s infinite;
+        }
+        .logo-clip-container .decoration.decoration-3 .pattern-2 {
+          animation: anim-pattern-2 0.6s 0.2s infinite;
+        }
+        .logo-clip-container .decoration.decoration-4 .pattern-2 {
+          animation: anim-pattern-2 0.6s infinite;
+        }
+        .logo-clip-container .decoration.decoration-5 .pattern-2 {
+          animation: anim-pattern-2 0.6s 0.4s infinite;
+        }
+        @keyframes anim-pattern-2 {
+          0%, 50% {
+            opacity: 0;
+          }
+          51%, 100% {
+            opacity: 1;
+          }
+        }
+        `}
+      </style>
+      <div className="logo-clip-container">
+        <div className="logo">
+          <div className="line">
+            <span className="box" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box" />
+          </div>
+          <div className="line">
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active-1" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active-1" />
+            <span className="box active" />
+            <span className="box active" />
+          </div>
+          <div className="line">
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active-1" />
+            <span className="box active-1" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+          </div>
+          <div className="line">
+            <span className="box" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box active" />
+            <span className="box" />
+          </div>
+        </div>
+        <div className="decoration decoration-1">
+          <div className="decoration pattern-1">
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box active-2" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+          </div>
+          <div className="decoration pattern-2">
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+          </div>
+        </div>
+        <div className="decoration decoration-2">
+          <div className="decoration pattern-1">
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box active-2" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+          </div>
+          <div className="decoration pattern-2">
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+          </div>
+        </div>
+        <div className="decoration decoration-3">
+          <div className="decoration pattern-1">
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box active-2" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+          </div>
+          <div className="decoration pattern-2">
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+          </div>
+        </div>
+        <div className="decoration decoration-4">
+          <div className="decoration pattern-1">
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box active-2" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+          </div>
+          <div className="decoration pattern-2">
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+          </div>
+        </div>
+        <div className="decoration decoration-5">
+          <div className="decoration pattern-1">
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box active-2" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+          </div>
+          <div className="decoration pattern-2">
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+            <div className="line">
+              <span className="box" />
+              <span className="box active-2" />
+              <span className="box" />
+            </div>
+            <div className="line">
+              <span className="box active-2" />
+              <span className="box" />
+              <span className="box active-2" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
